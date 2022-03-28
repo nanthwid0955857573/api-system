ef785e7c4a4bb51024f6f29744227a8767432411
Pb030638 <nanthwid101@gmail.com>
2022-03-28 23:31:33 +0700

    create model for new api

---
+++ app/models/student.model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+    const student = sequelize.define("student", {
+       id:{
+           type: Sequelize.INTEGER,
+           primaryKey:true
+       },
+       s_id: {
+           type: Sequelize.STRING
+       },
+       s_major:{
+        type: Sequelize.STRING
+       },
+       s_faculty:{
+        type: Sequelize.STRING
+       },
+       s_group:{
+        type: Sequelize.STRING
+       }
+    });
+};
